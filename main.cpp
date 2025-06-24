fe9baf452d141d51db769182b38c4defcc4ed693
Kris J <104223005+KrisoMoriso@users.noreply.github.com>
2025-06-24 23:33:50 +0200

    split_string works

--- main.cpp
+++ main.cpp
@@ -6,17 +6,22 @@
 #include "eval_parentheses.h"
 
 
+
+
 int main()
 {
     std::string usr_inp = "((2+3+4))";
     //std::cin >> usr_inp;
     clock_t tStart = clock();
-
-    //split_string_return_struct return_struct = split_string(usr_inp);
-
-   
-    
-
+    split_string_return_struct return_struct = split_string(usr_inp);
+    std::vector<double>& Nums = return_struct.Nums;
+    std::vector<char>& Ops = return_struct.Ops;
+    for (std::vector<double>::iterator it = Nums.begin(); it != Nums.end(); ++it) {
+        std::cout << *it << '\n';
+    }
+    for (std::vector<char>::iterator it = Ops.begin(); it != Ops.end(); ++it) {
+        std::cout << *it << '\n';
+    }
     
     printf("Time taken: %.2fs\n", (double)(clock() - tStart) / CLOCKS_PER_SEC);
     return 0;
--- split_string.cpp
+++ split_string.cpp
@@ -1,79 +1,37 @@
 #include <iostream>
 #include "split_string.h"
+#include <vector>
+#include <string>
 
-//split_string_return_struct split_string(std::string usr_inp) {
-//
-//    int num_start_index;
-//    bool did_enc_num = false;
-//    bool is_last_iter_op = true;
-//    int num_end_index;
-//
-//    double* pNums = NULL;
-//    int nums_to_set = 0;
-//    char* pOps = NULL;
-//    int ops_to_set = 0;
-//
-//    int nums_size = 0;
-//    int ops_size = 0;
-//    for (int i = 0; i < usr_inp.length(); i++) {
-//        if (usr_inp[i] == '+' or usr_inp[i] == '-' or usr_inp[i] == '/' or usr_inp[i] == '*' or usr_inp[i] == '(' or usr_inp[i] == ')') {
-//            ops_size++;
-//        }
-//        else {
-//
-//            nums_size++;
-//        }
-//    }
-//
-//    pNums = new double[nums_size];
-//    pOps = new char[ops_size];
-//
-//     
-//    for (int i = 0; i <= usr_inp.length(); i++) {
-//        if (usr_inp[i] == '+' or usr_inp[i] == '-' or usr_inp[i] == '/' or usr_inp[i] == '*' or usr_inp[i] == '(' or usr_inp[i] == ')' or i == usr_inp.length()) {
-//            if (!(i == usr_inp.length())) {
-//                pOps[ops_to_set] = usr_inp[i];
-//                ops_to_set++;
-//            }
-//            did_enc_num = false;
-//            if (!is_last_iter_op) {
-//                int num_length = (num_end_index - num_start_index);
-//                char* pTemp;
-//
-//
-//                pTemp = new char[num_length + 1];
-//                for (int i = 0; i <= num_length; i++) {
-//                    pTemp[i] = usr_inp[i + num_start_index];
-//                }
-//                pTemp[num_length + 1] = ' ';
-//
-//                pNums[nums_to_set] = std::atof(pTemp);
-//                nums_to_set++;
-//
-//            }
-//            is_last_iter_op = true;  
-//        }
-//        else  {
-//            if (!did_enc_num) {
-//                num_start_index = i;
-//                num_end_index = i;
-//                did_enc_num = true;
-//
-//            }
-//            else {
-//                num_end_index = i;
-//
-//            }
-//            is_last_iter_op = false;
-//        }
-//    }
-//
-//    
-//    split_string_return_struct return_struct;
-//    return_struct.pNums = pNums;
-//    return_struct.pOps = pOps;
-//    return_struct.nums_size = nums_size;
-//    return_struct.ops_size = ops_size;
-//
-//    return return_struct;
-//}
+
+split_string_return_struct split_string(std::string usr_inp) {
+
+	std::vector<double> Nums;
+	std::vector<char> Ops;
+	bool did_enc_num = false;
+	bool is_last_it_num = false;
+	std::string toDouble = "";
+	double temp_eval = 0;
+	int cos = usr_inp.length();
+
+	for (int i = 0; i < usr_inp.length(); i++) {
+		if (usr_inp[i] == '+' or usr_inp[i] == '-' or usr_inp[i] == '*' or usr_inp[i] == '/' or usr_inp[i] == '(' or usr_inp[i] == ')') {
+			Ops.push_back(usr_inp[i]);
+			if (is_last_it_num) {
+				temp_eval = std::stod(toDouble);
+				Nums.push_back(temp_eval);
+				toDouble = "";
+			}
+			is_last_it_num = false;
+		}
+		else {
+			is_last_it_num = true;
+			toDouble.push_back(usr_inp[i]);
+		}
+	}
+
+	split_string_return_struct return_struct;
+	return_struct.Nums = Nums;
+	return_struct.Ops = Ops;
+	return return_struct;
+}
